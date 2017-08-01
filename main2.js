289e8bfd56f591e124157dd30517e6d7853a2ed7
Youssef Saad <yousseffhs@yahoo.com>
2017-08-01 00:01:16 -0500

    mline regex added for js, jsx, sass, scss comments along with modifying regex for empty lines

--- main2.js
+++ main2.js
@@ -2,24 +2,29 @@ fs = require('fs')
 
 const mfile = "myfile.txt"
 
+  const removeables = {
+      html: /<!--(.*?[\s\S]*)-->/gm,
+      inline: /\/\/(.*)?/g,
+      console: /(console.*)/g,
+      empty: /^\s*$/gm,
+    }
+
 // async and wait currently not working
 async function readAndProcess() {
-  let content = await fs.readFile(mfile, 'utf8')
-
-  const removeables = {
-    html: /<!--(.*?[\s\S]*)-->/gm,
-    inline: /\/\/(.*)?/g,
-    console: /(console.*)/g,
-    empty: /^\s*$/gm,
+  let content
+  try {
+    content = await fs.readFile(mfile, 'utf8')    
+    return content;
   }
-
-  const result = content.replace(removeables.html, '').replace(removeables.inline, '').replace(removeables.console, '').replace(removeables.empty, '')
-
-  return result
+  catch(error){
+    console.log(error)
+  }
+  
 
 }
 
 
-readAndProcess.then(v => {
-  console.log(v);
+readAndProcess.then(content => {
+
+    const result = content.replace(removeables.html, '').replace(removeables.inline, '').replace(removeables.console, '').replace(removeables.empty, '')    
 })
