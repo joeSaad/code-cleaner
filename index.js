c21e8022f575c5e7b88c044b9cda82061a576078
Youssef Saad <yousseffhs@yahoo.com>
2017-08-10 16:00:23 -0500

    - removed testing folders - cleaned in code - added use to colors npm package for better visibility of messaging

--- index.js
+++ index.js
@@ -8,85 +8,12 @@ const readFileAsync = promisify(fs.readFile)
 const writeFileAsync = promisify(fs.writeFile)
 
 const program = require('commander')
+const colors = require('colors')
 const pkg = require('./package.json');
 
 let dirToClean
 
-function analyzeInput() {
-	let directoryPresent = false
-	if (process.argv.length > 1) {
-		if (process.argv.filter(t=> t.startsWith('-'))[0]) {
-			let flag = process.argv.filter(t=> t.startsWith('-'))[0]
-			let flag_i = process.argv.indexOf(flag)
-			switch(flag) {
-			    case '-h':
-			        console.log('This package helps you clean your code\n')
-			        console.log('-d helps you determine the directory\n')
-			        break;
-			    case '-v':
-			        console.log(pkg.version)
-			        break;
-			    case '-d':
-					if (process.argv.length < 4) {
-						console.log('ERROR - Please enter directory')
-						break
-					}
-					directoryPresent = true
-					dirToClean = path.resolve(__dirname,process.argv[2])
-			        break;
-			    default:
-					directoryPresent = true
-					dirToClean = path.resolve(__dirname,process.argv[2])	
-			        break;
-			}
-			process.argv.splice(flag_i, 1)
-		}
-		if (process.argv.indexOf('.') > -1) {
-			directoryPresent = true
-			dirToClean = __dirname
-		}
-	}	
-	return directoryPresent
-}
-
-//analyzeInput()
-
-/*if (analyzeInput()) {
-	console.log('process.argv.length : '+process.argv.length);
-	console.log('directoryPresent : '+directoryPresent);
-}
-else {
-	if (process.argv.length < 3) {
-		console.log('name of file not entered')
-		return
-	}
-	else {
-		console.log('process.argv.length : '+process.argv.length);
-		const fileToClean = process.argv[2]
-		console.log('fileToClean : '+fileToClean);
-	}
-}*/
-
 const userInput = process.argv;
-
-
-/*switch(userInput){
-	case this.includes('-v'):
-		console.log(pkg.version)
-		break;
-	case this.includes('-d'):
-		if (userInput.length < 4) {
-			console.log('ERROR - Please enter directory')
-			break
-		}
-		dirToClean = path.resolve(__dirname,process.argv[2])
-		break;
-	case this.includes('.'):
-		dirToClean = __dirname
-		break
-	default: 
-		console.log('file is')
-}*/
 
 program
 	.version(pkg.version)
@@ -99,30 +26,14 @@ program
 	if (program.directory) {
 		dirToClean = process.cwd()
 		cleanByDirectory(dirToClean)
-		console.log('  - directory: '+ dirToClean)
+		console.log(`Files at ${dirToClean} are cleaned 👍🏼`.bgGreen)
+
 	}
 	else{
 		const fileToClean = process.argv[2]
-		console.log('fileToClean : '+fileToClean);
 		cleanFile(fileToClean)
+		console.log(`File ${fileToClean} cleaned 👍🏼`.bgGreen)
 	}
-
-
-
-/*console.log('userInput.length : '+userInput.length);
-console.log('process.argv : '+process.argv);
-console.log('dirToClean : '+dirToClean);
-*/
-/*if (process.argv.includes('-d')) {
-	dirToClean = path.resolve(__dirname,process.argv.slice(3,4)[0])	
-	console.log('dirToClean : '+dirToClean);
-	cleanByDirectory(dirToClean)
-}
-else {
-	const fileToClean = process.argv.slice(2,3)[0]
-	cleanFile(fileToClean)
-}
-*/
 
 const removeables = {
 	html: /<!--(.*?[\s]*)-->/gm,
@@ -176,8 +87,6 @@ function cleanByDirectory(dir) {
 		const rf = path.resolve(dir,f);
 		cleanFile(rf)
 		})
-	console.log('\x1b[32m%s\x1b[0m', 'files cleaned')
-	
 }
 
 function readDirR(dir) {
